861e664ecd0bc8c7ab6200676581ae7d35793755
davidcsiki <david.csiki@gmail.com>
2016-02-29 17:53:46 +0100

    currncy

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // Fö modul definiálása.
-var superhero = angular.module( "superhero", [] );
+var superhero = angular.module( "superhero", ['currencyModule'] );
 
 superhero.controller( "nameController", [ "$scope", function( $scope ) {
     $scope.yourName = "Joe";
